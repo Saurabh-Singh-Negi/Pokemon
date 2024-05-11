170f11113f51fdec9de6fb08eec5050df12ec765
saurabh <saurabhnegi0007@gmail.com>
2024-05-11 18:10:35 +0530

    iteration 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import PokemonDetails from "./pages/PokemonDetails";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemondetails/:id" element={<PokemonDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
